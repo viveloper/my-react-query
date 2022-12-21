fd1ac132960c9be03c4ec28bce6606abf881df57
viveloper <ucsik1234@gmail.com>
2022-12-21 22:25:43 +0900

    feat: add components

---
+++ src/components/Posts.jsx
@@ -0,0 +1,11 @@
+const Posts = ({ posts }) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,5 @@
+const UserDetail = ({ user }) => {
+  return <div>{user.name}</div>;
+};
+
+export default UserDetail;
